f9701116ed3bbadc5fb4d132c652f49e3a3f7435
MrinalSisodia <mrinalsisodia28@gmail.com>
2025-05-11 07:13:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
 const express = require("express")
 const app = express();
-const cors = require("cors");
-const corsOptions = {
-  origin: "*",
-  credentials: true,
-  optionSuccessStatus: 200,
-};
+
 
 app.use(cors(corsOptions));
 
